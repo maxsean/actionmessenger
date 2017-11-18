1b4741f770bc84fcb8839928808a5b6e78922c28
maxsean <xsma1990@gmail.com>
2017-11-18 15:52:04 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :destroy]
 
-  devise_for :users
-
   devise_scope :user do
     get "users/sign_out" => "devise/sessions#destroy"
   end
