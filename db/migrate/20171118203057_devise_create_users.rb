7c35bc4cc7f530cef99ca3b9af015c97461df016
maxsean <xsma1990@gmail.com>
2017-11-19 10:52:44 -0500

    added handle and icon num attributes to user

--- db/migrate/20171118203057_devise_create_users.rb
+++ db/migrate/20171118203057_devise_create_users.rb
@@ -3,6 +3,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :handle
+      t.string :icon_num
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20171118203057) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "handle"
+    t.string "icon_num"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
